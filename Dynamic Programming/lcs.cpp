088c07f1a207fbed6e9baa85046f95b228a8d4de
Mizanur Rahman RIyadh <mizanurriyadh@ymail.com>
2018-04-12 01:34:22 +0600

    Add files via upload

---
+++ Dynamic Programming/lcs.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define MAXC 1000
+using namespace std;
+
+char A[MAXC],B[MAXC];
+int lenA,lenB;
+int dp[MAXC][MAXC];
+bool visited[MAXC][MAXC];
+int calcLCS(int i,int j)
+{
+	if(A[i]=='\0' or B[j]=='\0')
+        return 0;
+	if(visited[i][j])
+        return dp[i][j];
+
+	int ans=0;
+	if(A[i]==B[j])
+        ans=1+calcLCS(i+1,j+1);
+	else
+	{
+		int val1=calcLCS(i+1,j);
+		int val2=calcLCS(i,j+1);
+		ans=max(val1,val2);
+	}
+	visited[i][j]=1;
+	dp[i][j]=ans;
+	return dp[i][j];
+}
+int main() {
+	scanf("%s%s",A,B);
+	lenA=strlen(A);
+	lenB=strlen(B);
+	printf("%d\n",calcLCS(0,0));
+	return 0;
+
+}
