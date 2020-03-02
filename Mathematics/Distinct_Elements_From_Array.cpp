38505f5aa5deae58eb2fc7a59660d1b07318a007
Mizanur Rahman RIyadh <mizanurriyadh@ymail.com>
2020-03-03 01:17:04 +0600

    Add files via upload

---
+++ Mathematics/Distinct_Elements_From_Array.cpp
@@ -0,0 +1,28 @@
+// Example program
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i, counter;
+    int a[] = {6, 10, 5, 4, 9, 20, 4, 6, 10, 3, 4};
+    int n = sizeof(a)/sizeof(a[0]);
+    
+    sort(a, a+n);
+    
+    cout<<"Distinct elements of the Array: ";
+    counter=0;
+    for(i=0; i<n; i++)
+    {
+        while(i<n-1 && a[i]==a[i+1])
+        {
+            i++;
+        }
+        
+        cout<<a[i]<<" ";
+        counter++;
+    }
+    
+    cout<<"\nThe number of distinct elements: "<<counter<<endl;
+}
